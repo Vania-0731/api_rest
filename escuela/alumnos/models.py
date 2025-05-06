9178b24741b2890f58a1648024460e368cf9a56a
Vania <sonalysifuentes@gmail.com>
2025-05-05 22:24:57 -0500

    models created

--- escuela/alumnos/models.py
+++ escuela/alumnos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo = models.EmailField()
+    fecha_nacimiento = models.DateField()
+    direccion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
